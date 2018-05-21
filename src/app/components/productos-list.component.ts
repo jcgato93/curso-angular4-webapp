908164fa14bfef46a422c1787f7361bdc10315c4
juan camilo castillo saucedo <jcgato93@hotmail.com>
2018-05-20 22:09:22 -0500

    link activo del nav

--- src/app/components/productos-list.component.ts
+++ src/app/components/productos-list.component.ts
@@ -15,7 +15,7 @@ export class ProductosListComponent{
         private _route: ActivatedRoute,
         private _router: Router
     ){
-        this.titulo= 'Listado de productos'
+        this.titulo= 'Listado de productos';
     }
 
 
