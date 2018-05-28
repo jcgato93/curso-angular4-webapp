43d2d43bd371bc9941192496224011b86897e083
Camilo.IT <npgcamilo@gmail.com>
2018-05-28 14:45:05 -0500

    Subir imagenes

--- src/app/components/producto-add.component.ts
+++ src/app/components/producto-add.component.ts
@@ -4,6 +4,7 @@ import { Router,ActivatedRoute, Params } from '@angular/router';
 
 import { ProductosService } from '../services/productos.service';
 import { Producto } from '../models/producto';
+import { GLOBAL } from '../services/global';
 
 
 @Component({
@@ -30,22 +31,57 @@ export class ProductoAddComponent{
 
 
 
-    onSubmit(){
-       console.log(this.producto);    
-       
-       this._productoService.addProducto(this.producto).subscribe(
-           response=>{
-                if(response.code == 200){
-                    this._router.navigate(['/productos']);
+    onSubmit() {
+        console.log(this.producto);
+
+        //si existe un archivo para cargar 
+        if (this.filesToUpload.length >= 1) {
+            this._productoService.makeFileRequest(GLOBAL.url + 'upload-file', [], this.filesToUpload).then((result) => {
+                console.log(result);
+
+                this.resultUpload = result;//captura la respueta              
+                this.producto.imagen = this.resultUpload.filename;//llena la propiedad imagen con la de la respuesta para poder asociar el nombre del documeto
+                
+                this.saveProducto();
+            },
+                (error) => {
+                    console.log(error)
                 }
-                else{
-                    console.log(response);
-                }
-           },
-           error =>{
-              console.log(<any>error); 
-           }
-       );
+            );
+        } else {//si no tiene archivos para guardar solo guarda el resto de las propiedades del producto
+            this.saveProducto();
+        }
     }
 
+    /**
+     * Guarda un producto
+     */
+    saveProducto(){
+        this._productoService.addProducto(this.producto).subscribe(
+            response=>{
+                 if(response.code == 200){
+                     this._router.navigate(['/productos']);
+                 }
+                 else{
+                     console.log(response);
+                 }
+            },
+            error =>{
+               console.log(<any>error); 
+            }
+        );
+    }
+
+
+
+    public filesToUpload;
+    public resultUpload; 
+    /**
+     * añade el archivo al array de filesToUpload
+     * @param fileInput 
+     */
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>> fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -32,4 +32,32 @@ export class ProductosService {
      return this._http.post(this.url+'productos',params,{headers: headers})
                         .map(res=> res.json());
    }
+
+
+   makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+     return new Promise((resolve, reject)=> {
+        var formData: any = new FormData();
+        var xhr = new XMLHttpRequest();
+
+        for (let i = 0; i < files.length; i++) {
+          formData.append('uploads[]',files[i],files[i].name) ;
+        
+        }
+
+        xhr.onreadystatechange= function(){
+          if(xhr.readyState == 4){
+            if(xhr.status == 200){
+              resolve(JSON.parse(xhr.response));
+            }
+            else{
+              reject(xhr.response);
+            }
+          }
+        };
+
+        xhr.open("POST",url,true);
+        xhr.send(formData);
+
+     });
+   }
 }
